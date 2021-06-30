62f3db3dfd184dd75b1465b4b60d75d4c2bd10de
ItsEvdok <semionaevdokimov@icloud.com>
2021-06-30 19:19:14 -0400

    ?

--- 20-React/01-Activities/01-Component-Map/Unsolved/src/components/List.js
+++ 20-React/01-Activities/01-Component-Map/Unsolved/src/components/List.js
@@ -3,7 +3,9 @@ import React from 'react';
 function List(props) {
   return (
     <ul className="list-group">
-      {/* Map over the groceries from props and for each item render <li> tags with the item's name */}
+      {props.groceries.map(item => {
+        <li key={item.id} className='list-group-item'>{item.name}</li>
+      })}
     </ul>
   );
 }
