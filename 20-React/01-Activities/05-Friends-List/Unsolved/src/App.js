66ce76083848bc6c1d1fabcb271fb1a704a4ca75
ItsEvdok <semionaevdokimov@icloud.com>
2021-06-30 21:05:57 -0400

    Update App.js

--- 20-React/01-Activities/05-Friends-List/Unsolved/src/App.js
+++ 20-React/01-Activities/05-Friends-List/Unsolved/src/App.js
@@ -1,5 +1,5 @@
 // Import 'useState' Hook from React
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FriendCard from './components/FriendCard';
 import Wrapper from './components/Wrapper';
 import friends from './friends.json';
@@ -8,18 +8,28 @@ import './App.css';
 function App() {
   // Using useState, declare a new state variable 'friendsList' and set it to the 'friends' array from 'friends.json'
   // Name the setter method 'setFriendsList'
-  // YOUR CODE HERE
-  //
+  const [ friendList,setFriendList ] = useState(friends);
 
   // Create a method 'removeFriend()' that takes in 'id' and filters through 'friendsList'
   // Update the state with the new friends list.
-  // YOUR CODE HERE
-  //
+  const removeFriend = id => {
+    const newList = friendsList.filter(friend => friends.id !== id);
+    setFriendList(newList);
+  }
 
   return (
     <Wrapper>
       <h1 className="title">Friends List</h1>
-      {/* Map through 'friendsList' and render a 'FriendCard' for each friend */}
+      {friendList.map( friend => {
+        <FriendCard
+          id={friend.id}
+          key={friend.key}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+          removeFriend={removeFriend}
+        />
+      })}
       {/* Pass in the 'removeFriend()' method and each property of a friend */}
     </Wrapper>
   );
